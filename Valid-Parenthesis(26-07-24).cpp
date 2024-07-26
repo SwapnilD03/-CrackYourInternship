5b88c394ca5a8c05c32a56861ca1e6b045336040
SwapnilD03 <swapnildas18403@gmail.com>
2024-07-26 11:13:48 +0530

    Create Valid-Parenthesis(26-07-24).cpp

---
+++ Valid-Parenthesis(26-07-24).cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+     stack<char> st;
+
+     for(int i=0;i<s.length();i++){
+        char ch=s[i];
+        
+        if(ch=='(' || ch=='{' || ch=='['){
+            st.push(ch);
+        }
+       
+        else if(!st.empty() && ( (ch==')' && st.top()=='(') || (ch=='}' && st.top()=='{') || (ch==']' && st.top()=='[') ) ){
+            st.pop();
+        }
+        else{
+            return false;
+        }
+     }
+     return st.empty();
+    }
+};
