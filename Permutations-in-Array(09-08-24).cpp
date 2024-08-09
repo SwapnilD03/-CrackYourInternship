3dd93e0b22957bbf0a91c278f3e23c33ecabfe7a
SwapnilD03 <swapnildas18403@gmail.com>
2024-08-09 20:01:39 +0530

    Create Permutations-in-Array(09-08-24).cpp

---
+++ Permutations-in-Array(09-08-24).cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool isPossible(int k, vector<int> &arr1, vector<int> &arr2) {
+        int n = arr1.size();
+        sort(arr1.begin(), arr1.end());
+        sort(arr2.rbegin(), arr2.rend());
+
+        for (int i = 0; i < n; i++) {
+            if (arr1[i] + arr2[i] < k) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
