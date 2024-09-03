637dcdeeba6c86b6e5f070858e4e1f77a50a7c04
SwapnilD03 <swapnildas18403@gmail.com>
2024-09-03 22:41:22 +0530

    Create Count-Different-Palindromic-Subsequences(03-09-24).cpp

---
+++ Count-Different-Palindromic-Subsequences(03-09-24).cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int countPalindromicSubsequences(string s) {
+        int n=s.length();
+        vector<vector<long long>> dp(n,vector<long long>(n));
+        const int mod=1000000007;
+        for(int i=n-1;i>=0;i--){
+            dp[i][i]=1;
+            for(int j=i+1;j<n;j++){
+                if(s[i]!=s[j]){
+                    dp[i][j]=dp[i+1][j]+dp[i][j-1]-dp[i+1][j-1];                
+                }
+                else{
+                    dp[i][j]=dp[i+1][j-1]*2;
+
+                    int left=i+1,right=j-1;
+
+                    while(left<=right && s[left]!=s[i]){
+                        left++;
+                    }
+                    while(left<=right && s[right]!=s[i]){
+                        right--;
+                    }
+
+                    if(left>right){
+                        dp[i][j]+=2;
+                    }
+                    else if(left==right){
+                        dp[i][j]+=1;
+                    }
+                    else{
+                        dp[i][j]-=dp[left+1][right-1];
+                    }
+                }
+                dp[i][j]=(dp[i][j]+mod)%mod;
+            }
+        }
+        return (int)dp[0][n-1];
+    }
+};
